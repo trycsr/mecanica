a944308c2273f0b470328b6fdd8f5a24e24ae022
Ryan Christian SR <83259648+trycsr@users.noreply.github.com>
2023-12-11 10:47:31 -0400

    Add files via upload

---
+++ database-memory.js
@@ -0,0 +1,39 @@
+import { randomUUID } from "crypto"
+
+
+export class DatabaseMemory{
+    #motors = new Map()
+
+list(search){
+    return Array.from(this.#motors.entries()).map((motorArray) => {
+        const id = motorArray[0]
+
+        const data = motorArray[1]
+
+        return{
+            id,
+            ...data,
+        }
+        
+    })
+    .filter(motor => {
+        if (search){
+        return motor.potencia.includes(search)
+        }
+        return true
+    })
+}
+
+    create(motor){
+        const motorId = randomUUID()
+        this.#motors.set(motorId, motor)
+    }
+    
+    update(id, motor){
+        this.#motors.set(id, motor)
+    }
+
+    delete(id, motor){
+        this.#motors.delete(id, motor)
+    }
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+import { fastify } from 'fastify'
+import {DatabaseMemory} from "./database-memory.js"
+
+const server = fastify()
+const database = new DatabaseMemory()
+
+server.post('/motor', (request, reply) => {
+   const {potencia, cilindro, tamanho} = request.body
+    database.create({
+        potencia: potencia,
+        cilindro: cilindro,
+        tamanho: tamanho
+    })
+    console.log(database.list())
+    return reply.status(201).send()
+})
+
+server.get('/motor', (request) => {
+    const search = request.query.search
+
+    console.log(search)
+    
+    const motor = database.list(search)
+   
+    return motor
+})
+
+server.put('/motor/:id', (request, reply) => {
+
+    const motorId = request.params.id
+    const {potencia, cilindro, tamanho} = request.body
+    const motor = database.update(motorId, {
+        potencia,
+        cilindro,
+        tamanho,
+    })
+    return reply.status(204).send()
+})
+
+server.delete('/motor/:id', (request, reply) => {
+    const motorId = request.params.id
+
+    database.delete(motorId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: 3333,
+})
